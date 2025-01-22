6bd385d7f59c96146c07d3d1b3b8e00ff8d063f5
Milan Vrbas <milan.vrbas1@gmail.com>
2025-01-22 21:05:27 +0100

    fix

--- Frontend/src/components/TeamPointsTable/TeamPointsTable.jsx
+++ Frontend/src/components/TeamPointsTable/TeamPointsTable.jsx
@@ -67,45 +67,54 @@ const TeamPointsTable = ({ campData, results, setResults, gameTypeId, setGameTyp
             <label>Upravte pořadí týmů přetažením</label>
 
             <DragDropContext onDragEnd={handleDragEnd}>
-                <Droppable droppableId="teamList">
-                    {(provided) => (
-                        <ul className="team-list" {...provided.droppableProps} ref={provided.innerRef}>
-                            {results.map((result, index) => (
-                                <Draggable key={result.team_name} draggableId={result.team_name} index={index}>
-                                    {(provided) => (
-                                        <li
-                                            className="team-list-item"
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                        >
-                                            <div className="team-info">
-                                                <span className="team-position">{result.position}.</span>
-                                                <div
-                                                    className="team-name"
-                                                    style={{ backgroundColor: getTeamColor(result.team_name) }}
-                                                >
-                                                    {result.team_name}
-                                                </div>
-                                            </div>
-                                            <input
-                                                type="number"
-                                                className="points-input"
-                                                value={result.points_awarded}
-                                                onChange={(e) =>
-                                                    handleManualPointChange(index, parseFloat(e.target.value) || 0)
-                                                }
-                                                min={0}
-                                            />
-                                        </li>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </ul>
-                    )}
-                </Droppable>
+                <table className="team-table">
+                    <thead>
+                        <tr>
+                            <th>Umístění</th>
+                            <th>Tým</th>
+                            <th>Body</th>
+                        </tr>
+                    </thead>
+                    <Droppable droppableId="teamList">
+                        {(provided) => (
+                            <tbody ref={provided.innerRef} {...provided.droppableProps}>
+                                {results.map((result, index) => (
+                                    <Draggable key={result.team_name} draggableId={result.team_name} index={index}>
+                                        {(provided) => (
+                                            <tr
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+
+                                            >
+                                                <td className="team-position">{result.position}.</td>
+                                                <td className="team-info" >
+                                                    <div className="team-name" style={{ backgroundColor: getTeamColor(result.team_name) }}>
+                                                        <span className="name">{result.team_name}</span>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <input
+                                                        type="number"
+                                                        className="points-input"
+                                                        value={result.points_awarded}
+                                                        onChange={(e) =>
+                                                            handleManualPointChange(index, parseFloat(e.target.value) || 0)
+                                                        }
+                                                        min={0}
+                                                    />
+                                                </td>
+                                            </tr>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </tbody>
+                        )}
+                    </Droppable>
+                </table>
             </DragDropContext>
+
         </div>
     );
 };
