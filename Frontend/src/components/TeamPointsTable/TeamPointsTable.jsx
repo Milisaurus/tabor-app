a32ab69f5a48a788a482f00bbb0f096033f66260
Milan Vrbas <milan.vrbas1@gmail.com>
2025-01-22 21:10:19 +0100

    Update TeamPointsTable.jsx

--- Frontend/src/components/TeamPointsTable/TeamPointsTable.jsx
+++ Frontend/src/components/TeamPointsTable/TeamPointsTable.jsx
@@ -66,7 +66,16 @@ const TeamPointsTable = ({ campData, results, setResults, gameTypeId, setGameTyp
 
             <label>Upravte pořadí týmů přetažením</label>
 
-            <DragDropContext onDragEnd={handleDragEnd}>
+            <DragDropContext
+    onDragStart={(start) => {
+        document.querySelectorAll("tr")[start.source.index + 1]?.classList.add("dragging-row");
+    }}
+    onDragEnd={(result) => {
+        document.querySelectorAll(".dragging-row").forEach((el) => el.classList.remove("dragging-row"));
+        handleDragEnd(result);
+    }}
+>
+
                 <table className="team-table">
                     <thead>
                         <tr>
